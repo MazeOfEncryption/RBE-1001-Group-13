1dff7239d83cba5556863d258f865fb4c40eb2a9
Maze <encryptedmaze@gmail.com>
2023-01-12 12:10:12 -0500

    Added  empty v5 project

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\Maze                                             */
+/*    Created:      Thu Jan 12 2023                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+    // Initializing Robot Configuration. DO NOT REMOVE!
+    vexcodeInit();
+}
