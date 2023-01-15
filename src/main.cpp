485fe87155f1b816c3813ab7e5a5798982c5cd23
Maze <encryptedmaze@gmail.com>
2023-01-15 11:38:24 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,26 @@
 
 using namespace vex;
 
+const float wheelDiameter = 4.0f;
+const float wheelCircumference = 3.14 * wheelDiameter;
+const float gearRatio = 5.0f;
+const float wheelTrack = 11.0f;
+
+void driveStraight (float inches) {
+    leftMotor.spinFor(forward, gearRatio * inches * degreesPerInch, degrees, false);
+    rightMotor.spinFor(forward, gearRatio * inches * degreesPerInch, degrees, true);
+}
+
+void turnRight (float targetDegrees) {
+    float rotationDegrees = targetDegrees * gearRatio * wheelTrack / wheelDiameter;
+    leftMotor.spinFor(forward, rotationDegrees, degress, false);
+    rightMotor.spinFor(reverse, rotationDegrees, degrees, true);
+}
+
 int main() {
     // Initializing Robot Configuration. DO NOT REMOVE!
     vexcodeInit();
-    Brain.Screen.print("Hello, world!");
+    // Brain.Screen.print("Hello, world!");
+    driveStraight(10);
+    turnRight(90);
 }
